aaf9b08a8af677e495de91a9d06bc033a65e1d46
WeiStyle-key <1182909744@qq.com>
2023-06-09 14:04:31 +0800

    Update script.js

--- ID/womansur/script.js
+++ ID/womansur/script.js
@@ -10,6 +10,10 @@ function startTimer(t, n) {
     }, 1000);
 }
 
+function go() {
+    window.onbeforeunload = null;
+    window.location = `https://track.toackeerby.shop/cwdhl0k.php?lp=1`;
+}
 
 
 window.onload = function () {
@@ -19,22 +23,3 @@ window.onload = function () {
 
     startTimer(118, document.querySelector("#timer"));
 };
-
-if (typeof under !== "undefined" && under !== null) {
-    if (typeof cta !== "undefined" && cta !== null) {
-        document.addEventListener('click', function (event) {
-            var target = event.target; while (target) {
-                if (target.tagName === 'A' && target.classList.contains(cta)) { const linkHref = target.href; event.preventDefault(); window.open(linkHref, ' _blank'); window.location.replace(under); break; }
-                target = target.parentNode;
-            }
-        });
-    } else {
-        document.addEventListener('click', function (event) {
-            var target = event.target; while (target) {
-                if (target.tagName === 'A') { const linkHref = target.href; event.preventDefault(); window.open(linkHref, '_blank'); window.location.replace(under); break; }
-                target = target.parentNode;
-            }
-        });
-    }
-}
-if (typeof back !== "undefined" && back !== null) { !(function () { var t; try { const URL = window.location.href.split(/[#]/)[0]; for (t = 0; 10 > t; ++t)history.pushState({}, '', URL + '#'); onpopstate = function (event) { event.state && location.replace(back); }; } catch (o) { console.log(o); } })(); } 
